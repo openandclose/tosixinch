8f6e96c18f82876d97129520bb0314889f7ab2bd
Open Close <openandclose23@gmail.com>
2019-02-04 01:56:37 +0900

    Fix class attribute regex
    It includes [0-9] (among other things).
    
    Actual spec.
    https://www.w3.org/TR/CSS22/syndata.html#characters

--- tosixinch/util.py
+++ tosixinch/util.py
@@ -657,7 +657,7 @@ def get_component_size(el, fname, stream=None):
 
 
 def transform_xpath(path):
-    pat = r'([a-zA-Z]+|[hH][1-6]|\*)\[@class==([\'"])([a-zA-Z-_]+)\2\]'
+    pat = r'([a-zA-Z]+|[hH][1-6]|\*)\[@class==([\'"])([_a-zA-Z0-9-]+)\2\]'
     pat = re.compile(pat)
     repl = r'\1[contains(concat(" ", normalize-space(@class), " "), " \3 ")]'
     return pat.sub(repl, path)
