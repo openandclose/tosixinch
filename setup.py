0f2a6f500ca7ee72d9c0c8ee637687c24a343001
Open Close <openandclose23@gmail.com>
2019-02-05 01:47:21 +0900

    Make Add Python3.7

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Text Processing :: Markup :: HTML',
         'Topic :: Utilities',
     ],
