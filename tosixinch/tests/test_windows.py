750b491d48ae9e2c3a0e7adb57b43499f6a87934
Open Close <openandclose23@gmail.com>
2019-03-20 04:54:39 +0900

    Refactor the order of classes
    The same as test_location.py.

--- tosixinch/tests/test_windows.py
+++ tosixinch/tests/test_windows.py
@@ -13,22 +13,6 @@ def use_ntpath(monkeypatch):
     monkeypatch.setattr(os, 'path', ntpath)
 
 
-class TestWindowsLocalReferenceRaw:
-
-    def compare(self, url, local_url, fname):
-        comp = location.Component(url, '.', platform='win32')
-        url = comp._remove_windows_chars(url)
-        assert comp._make_local_url(url) == local_url
-        assert comp._make_filename(url) == fname
-
-    def test(self):
-        url, local_url, fname = (
-            'aaa/bbb?cc',
-            'aaa/bbb_cc',
-            r'aaa\bbb_cc')
-        self.compare(url, local_url, fname)
-
-
 class TestWindowsMakePath:
 
     def compare(self, url, fname, fnew):
@@ -48,3 +32,19 @@ class TestWindowsMakePath:
             r'C:\aaa.org\bbb.html',
             r'_htmls\C\aaa.org\bbb--extracted.html')
         self.compare(url, fname, fnew)
+
+
+class TestWindowsLocalReferenceRaw:
+
+    def compare(self, url, local_url, fname):
+        comp = location.Component(url, '.', platform='win32')
+        url = comp._remove_windows_chars(url)
+        assert comp._make_local_url(url) == local_url
+        assert comp._make_filename(url) == fname
+
+    def test(self):
+        url, local_url, fname = (
+            'aaa/bbb?cc',
+            'aaa/bbb_cc',
+            r'aaa\bbb_cc')
+        self.compare(url, local_url, fname)
